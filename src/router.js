5bfac078fd3a38c795eac0f8596c8e71f7ff3e30
METROH <alejandrodesss@gmail.com>
2023-09-11 21:54:27 -0300

    lpm

---
+++ src/router.js
@@ -0,0 +1,53 @@
+import { main_section } from "./constants/dom.js";
+import table from "./components/table.js";
+
+function renderContent(route) {
+  const contentMap = {
+    //Primer elemento de los objetos es el title (La wea que esta al lado del favicon), el segundo es el contenido.
+    "/": ["Home", "Bienvenido al Club de Golf"], //[titulo la pesa, contenido de la wea]
+    "/home": ["Home", "Welcome XD"],
+    "/games": ["Games", "Juegos de Golf"],
+    "/ranking": ["Ranking", "Ranking de Jugadores"],
+    "/table": ["Table view", table()],
+    "/perfil": ["WOM", "Perfil XD"],
+  };
+  const [title, content] = contentMap[route] || [
+    "Página no encontrada",
+    "Página no encontrada XD",
+  ];
+  document.title = title;
+  if (content instanceof HTMLElement) {
+    //El contenido es un elemento del DOM, lo añadimos al main_section
+    main_section.innerHTML = "";
+    main_section.append(content);
+  } else {
+    // Si no, mostramos el contenido como HTML
+    main_section.innerHTML = content;
+  }
+}
+
+function handleRouteChange() {
+  const path = window.location.pathname; // Obtiene la ruta actual
+  renderContent(path);
+}
+
+export function initializeRouter() {
+  // Escuchar el evento popstate para detectar cambios en la URL
+  window.addEventListener("popstate", handleRouteChange);
+
+  // Inicializar la aplicación al cargar la página
+  document.addEventListener("DOMContentLoaded", handleRouteChange);
+}
+
+export function handleLinkClick(link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault(); // Evita la acción predeterminada del enlace (navegar a una nueva página)
+    const path = this.getAttribute("href"); // Obtiene la ruta del enlace
+
+    // Cambiar la URL sin recargar la página
+    history.pushState(null, null, path);
+
+    // Renderizar el contenido basado en la nueva ruta
+    renderContent(path);
+  });
+}
