777fc032b86058e4e46adc86f4b8e785f3198b62
lerodre <alejandro.rivera1@mail.udp.cl>
2023-10-09 21:00:53 -0300

    hola

--- server/requests/controllers/playerController.js
+++ server/requests/controllers/playerController.js
@@ -17,6 +17,37 @@ export function getAll(request, response){
 }
 
 export function getOne(request, response){
-    
+
+    const user_id = request.body
+    const user_data = get_user_data(user_id)
     console.log("Codigo para obtener solo un usuario")
+    response.send(user_data)
+}
+
+function get_user_data(user_data){
+
+    const get_user_query = `--sql
+    SELECT * users WHERE id = ${user_data.id};
+    `
+
+    const db = new sqlite3.Database(database_path, (error) => {
+        
+        if(error){
+          console.log('Error: al acceder a la base de datos.')
+          errors++
+        }
+    });
+    
+    const user = db.run(get_user_query, (error) => {
+        
+        if(error){
+          console.log("Error: failed to get user from the database.")
+          errors++
+          return "Error: failed to get user from the database."
+        } 
+    });
+
+    const JSON_user = JSON.stringify(user)
+    console.log(JSON_user)    
+    return JSON_user
 }
